c2f1bb1663e4be46bd2e38460992c735c560ce78
I-AM-Ramya <155509135+I-AM-Ramya@users.noreply.github.com>
2024-01-16 18:38:59 +0530

    #include<stdio.h> int main() {     int n,j,i;     scanf("%d",&n);     for (i=n; i>=1; i--)     {         for (j=i; j>=1; j--)         {             printf("%c ",64+i);         }         printf(" ");     } }

---
+++ Basic_Pattern_3.4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,j,i;
+    scanf("%d",&n);
+    for (i=n; i>=1; i--)
+    {
+        for (j=i; j>=1; j--)
+        {
+            printf("%c ",64+i);
+        }
+        printf("
+");
+    }
+}
